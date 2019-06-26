c99eae7ed07b36f95b0935cf58b24748c1d0219c
Olga Pereverzeva <olja125@yandex.ru>
2019-06-26 10:23:45 +0300

    update helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,7 @@ module ApplicationHelper
     # Возвращает полный заголовок на основе заголовка страницы.
     def full_title(page_title='')
         base_title="Ruby on Rails Tutorial Sample App"
-        if page_title.empty?
-            base_title
-        else 
-            page_title + " | " + base_title
-        end
+        return base_title if page_title.empty?
+        return "#{page_title} | #{base_title}"
     end
 end
