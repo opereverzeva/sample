15d84e57e94eced4162a7999530df6323f743e8a
Olga Pereverzeva <olja125@yandex.ru>
2019-07-15 17:51:09 +0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'about' =>'static_pages#about'
   get 'contact' =>'static_pages#contact'
   get 'signup' => 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
